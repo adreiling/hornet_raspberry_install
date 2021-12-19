f2172f16100d61870875dd1b4049512623db89fd
Augusto Dreiling <37916158+adreiling@users.noreply.github.com>
2021-12-19 19:28:40 -0300

    Add files via upload

---
+++ Install_Iot2tangleStreamsHTTPGateway.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+echo "=================================================================="
+echo "Installing RUST"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo "=================================================================="
+echo "Installing build dependencies"
+apt -y update
+apt install build-essential pkg-config libssl-dev
+
+echo "=================================================================="
+echo "Downloading the Repository iot2tangle Streams-HTTP-Gateway"
+git clone https://github.com/iot2tangle/Streams-http-gateway.git
+
+echo "=================================================================="
+echo "Run the streams-gateway"
+cargo run --release
