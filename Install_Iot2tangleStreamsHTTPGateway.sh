4e6d9bc7b4f6da0f6734687fa54345282c13c477
Augusto Dreiling <37916158+adreiling@users.noreply.github.com>
2021-12-19 21:03:52 -0300

    Update Install_Iot2tangleStreamsHTTPGateway.sh
    Adding cargo

--- Install_Iot2tangleStreamsHTTPGateway.sh
+++ Install_Iot2tangleStreamsHTTPGateway.sh
@@ -5,6 +5,10 @@ echo "Installing RUST"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 echo "=================================================================="
+echo "Installing Cargo"
+apt-get install -y cargo
+
+echo "=================================================================="
 echo "Installing build dependencies"
 apt -y update
 apt install build-essential pkg-config libssl-dev
